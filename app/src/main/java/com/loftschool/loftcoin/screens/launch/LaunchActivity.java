5e2784116aaf9919a7bd9f26fd3cff0334fb7fea
Dimas <droxdtv@mail.ru>
2019-03-17 17:05:59 +0800

    add Prefs and App

---
+++ app/src/main/java/com/loftschool/loftcoin/screens/launch/LaunchActivity.java
@@ -0,0 +1,13 @@
+package com.loftschool.loftcoin.screens.launch;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
