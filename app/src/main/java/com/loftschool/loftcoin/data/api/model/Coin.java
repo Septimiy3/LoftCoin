fa0dd35ae25ff386eb096f6e3fda1b6dc257762c
Dimas <droxdtv@mail.ru>
2019-03-25 23:41:15 +0800

    create Start Presenter and add animation start activity

---
+++ app/src/main/java/com/loftschool/loftcoin/data/api/model/Coin.java
@@ -0,0 +1,27 @@
+package com.loftschool.loftcoin.data.api.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Map;
+
+public class Coin {
+
+    @SerializedName("id")
+    public int id;
+
+    @SerializedName("name")
+    public String name;
+
+    @SerializedName("symbol")
+    public String symbol;
+
+    @SerializedName("slug")
+    public String slug;
+
+    @SerializedName("last_updated")
+    public long lastUpdated;
+
+    @SerializedName("quote")
+    public Map<String, Quote> quote;
+
+}
