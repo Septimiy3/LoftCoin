c714855d2086829414f056bee2df787170e33e30
Dimas <droxdtv@mail.ru>
2019-04-14 23:10:08 +0800

    Add transaction wallets

--- app/src/main/java/com/loftschool/loftcoin/data/db/room/WalletDao.java
+++ app/src/main/java/com/loftschool/loftcoin/data/db/room/WalletDao.java
@@ -1,6 +1,8 @@
 package com.loftschool.loftcoin.data.db.room;
 
 
+import com.loftschool.loftcoin.data.db.model.Transaction;
+import com.loftschool.loftcoin.data.db.model.TransactionModel;
 import com.loftschool.loftcoin.data.db.model.Wallet;
 import com.loftschool.loftcoin.data.db.model.WalletModel;
 
@@ -18,7 +20,13 @@ public interface WalletDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void saveWallet(Wallet wallet);
 
-
     @Query("SELECT w.*, c.* FROM Wallet w, Coin c WHERE w.currencyId = c.id ")
     Flowable<List<WalletModel>> grtWallets();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void saveTransactions(List<Transaction> transactions);
+
+    @Query("SELECT t.*,c.* FROM `Transaction` t,Coin c WHERE t.walletId = :walletId AND t.currencyId = c.id ORDER BY t.date DESC")
+    Flowable<List<TransactionModel>> getTransaction(String walletId);
+
 }
